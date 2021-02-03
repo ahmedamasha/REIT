a7374fe4a6f4dfbb45d39eac4b2ae25a65495c13
Ahmed Amasha <ahmed.amasha@it-admins-MacBook-Pro.local>
2021-02-03 21:51:16 +0400

    init the repo

---
+++ src/main/java/com/reit/reit/respositry/ActivityRepository.java
@@ -0,0 +1,7 @@
+package com.reit.reit.respositry;
+
+import com.reit.reit.model.Activity;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ActivityRepository extends PagingAndSortingRepository<Activity, Long> {
+}
---
+++ src/main/java/com/reit/reit/respositry/BuildingRepository.java
@@ -0,0 +1,7 @@
+package com.reit.reit.respositry;
+
+import com.reit.reit.model.Building;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface BuildingRepository extends PagingAndSortingRepository<Building, Long> {
+}
